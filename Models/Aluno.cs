95afe15b61961e7b33bcf49a04b7b53e3ce04fe3
Lucas Martins Guimarães <lucas_10011@hotmail.com>
2021-02-09 20:06:44 -0300

    Criação da primeira versão do Cadastro

---
+++ Models/Aluno.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Ugb.Models
+{
+    [Table("Alunos")]
+    public class Aluno
+    {
+        [Key]
+        public int PersonId;
+
+        [Display(Name = "Nome")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Curso")]
+        [Required]
+        public string Course { get; set; }
+
+        [Display(Name = "Matricula")]
+        [Required]
+        public string StudentRegistration { get; set; }
+    
+        [Display(Name = "Rua")]
+        public string Street { get; set; }
+
+        [Display(Name = "Bairro")]
+        public string Neighborhood { get; set; }
+
+        [Display(Name = "Estado")]
+        public string State { get; set; }
+
+        [Display(Name = "Telefone")]
+        [Required]
+        public string StatCellphonee { get; set; }
+
+        [Display(Name = "Telefone")]
+        public string Telephone { get; set; }
+
+        [Display(Name = "Data")]
+        public string Data { get; set; }
+
+        [Display(Name = "Data de Nascimento")]
+        [Required]
+        public string Birthday { get; set; }
+
+        [Display(Name = "Email")]
+        [Required]
+        public string Email { get; set; }
+    }
+}
---
+++ Models/UgbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace Ugb.Models
+{
+    public class UgbContext : DbContext
+    {
+        public UgbContext() : base("Name=AlunosDb")
+        {
+            Database.SetInitializer<UgbContext>(
+                new CreateDatabaseIfNotExists<UgbContext>());
+            Database.Initialize(false);
+
+            Database.Log = d => System.Diagnostics.Debug.WriteLine(d);
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+
+    }
+}
